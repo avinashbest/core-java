5c86e98584dff47ae65e506359950bc696d3c5e2
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 20:57:48 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.avinashbest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+    }
+}
