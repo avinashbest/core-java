916347f74551d107a2355249d2ac93db4c0fb32b
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 22:25:50 +0530

    The Math Class

--- Main.java
+++ Main.java
@@ -3,17 +3,14 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        /*Implicit Casting*/
-        /*byte > short > int > long > float > double*/
-        short x = 1;
-        int y = x + 2;
-        System.out.println(y);
-        /*Explicit Casting*/
-        double a = 1.1;
-        int b = (int) (a + 2);
-        System.out.println(b);
-        /*Parsing String in Integer*/
-        String str = "123";
-        System.out.println(Integer.parseInt(str));
+        System.out.println(Math.round(1.2F));
+        System.out.println(Math.round(1.2));
+        System.out.println(Math.ceil(1.2));
+        System.out.println(Math.floor(1.2));
+        System.out.println(Math.max(200, 300));
+        System.out.println(Math.min(200, 300));
+        System.out.println(Math.random());//by default return a doubleType random number b/w 0 to 1
+        System.out.println(Math.round(Math.random() * 100));
+        System.out.println((int) (Math.random() * 100));
     }
 }
