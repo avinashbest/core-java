22866a7db80525a103ed5c47aabb1e4ac4ec2545
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:41:51 +0530

    Multi-Arrays

--- Main.java
+++ Main.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] number = {2, 4, 6, 8, 10};
-        System.out.println(number.length);
-        Arrays.sort(number);
-        System.out.println(Arrays.toString(number));
+        int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
