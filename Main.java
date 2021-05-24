100361d8ede18e0e5f996de11a10a6b7edcb70cd
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:30:40 +0530

    Escape Sequences

--- Main.java
+++ Main.java
@@ -5,20 +5,14 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
+        String message1 = "Hello \"Avinash\"";
+        String message2 = "C:\\Windows\\...";
+        String message3 = "C:\nWindows\n...";
+        String message4 = "C:\tWindows\t...";
 
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("l"));
-        System.out.println(message.indexOf("sky"));
-        System.out.println(message.replace("!", "*"));
-        System.out.println(message.toLowerCase(Locale.ROOT));
-        System.out.println(message.toUpperCase(Locale.ROOT));
-        System.out.println(message.trim()) ; /*To remove unnecessary space.*/
-
-        /*NOTE: IN JAVA, ALL THE METHOD WHICH PERFORM OPERATION ON STRING RETURNS A NEW STRING.*/
-        /*ALL THE STRING IN JAVA ARE IMMUTABLE*/
-        System.out.println(message);
+        System.out.println(message1);
+        System.out.println(message2);
+        System.out.println(message3);
+        System.out.println(message4);
     }
 }
