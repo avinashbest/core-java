93b74091ed43d4a42c4152957ed8cc2491fa8c54
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:05:11 +0530

    Primitive Data types

--- Main.java
+++ Main.java
@@ -3,7 +3,10 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World");
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
     }
 }
