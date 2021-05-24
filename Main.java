0e8d4f379674de815f463f59f641ee6f55cffbb7
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:26:47 +0530

    String

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 package com.avinashbest;
 
-import java.awt.*;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("l"));
+        System.out.println(message.indexOf("sky"));
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message.toLowerCase(Locale.ROOT));
+        System.out.println(message.toUpperCase(Locale.ROOT));
+        System.out.println(message.trim()) ; /*To remove unnecessary space.*/
+
+        /*NOTE: IN JAVA, ALL THE METHOD WHICH PERFORM OPERATION ON STRING RETURNS A NEW STRING.*/
+        /*ALL THE STRING IN JAVA ARE IMMUTABLE*/
+        System.out.println(message);
     }
 }
