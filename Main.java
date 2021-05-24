8fc8d109764421a1b6646f854b161aec42c305db
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 22:00:38 +0530

    Increment & Decrement Operator

--- Main.java
+++ Main.java
@@ -3,15 +3,27 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        int result = 10 + 3;
-        System.out.println(result);
-        result = 10 - 3;
-        System.out.println(result);
-        result = 10 / 3;
-        System.out.println(result);
-        result = 10 * 3;
-        System.out.println(result);
-        result = 10 % 3;
-        System.out.println(result);
+        int x = 1;
+        x++;
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+
+        x = 1;
+        int y = x++; // post-increment
+        System.out.println(x + "\t" + y);
+        y = ++x; //pre-increment
+        System.out.println(x + "\t" + y);
+
+        x += 2; //x = x + 2
+        System.out.println(x);
+        x -= 2; //x = x - 2
+        System.out.println(x);
+        x *= 2; //x = x * 2
+        System.out.println(x);
+        x /= 2; //x = x / 2
+        System.out.println(x);
+        x %= 2; //x = x % 2
+        System.out.println(x);
     }
 }
