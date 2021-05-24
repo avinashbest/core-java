7dc851b184f7795e0f44bab7a81a6b24d16ca770
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:55:57 +0530

    Arithmetic Operator

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
 package com.avinashbest;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(Arrays.deepToString(numbers));
+        int result = 10 + 3;
+        System.out.println(result);
+        result = 10 - 3;
+        System.out.println(result);
+        result = 10 / 3;
+        System.out.println(result);
+        result = 10 * 3;
+        System.out.println(result);
+        result = 10 % 3;
+        System.out.println(result);
     }
 }
