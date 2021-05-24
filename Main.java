ea637940739e16a0e2b5eff460b3ebcc66527672
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:09:43 +0530

    Reference Data types

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 package com.avinashbest;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
