ea549bd4c9698fda43569a09f6b9b55bbfb80303
Avinash Kumar <avinashbest3@gmail.com>
2021-05-24 21:13:43 +0530

    Reference Data type memory allocation behaviour

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 package com.avinashbest;
 
-import java.util.Date;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
