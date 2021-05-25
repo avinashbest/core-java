0343386c78da18998e0e60716dcd0e7a2b41b580
Avinash Kumar <avinashbest3@gmail.com>
2021-05-25 12:11:10 +0530

    if-statements

--- Main.java
+++ Main.java
@@ -3,15 +3,25 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        int temperature = 22;
-        boolean isWarm = temperature > 20 && temperature < 30;
-        System.out.println(isWarm);
-
-        boolean hasHighIncome = true;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = false;
-        boolean isEligible = (hasGoodCredit || hasHighIncome) && !hasCriminalRecord;
-        System.out.println(isEligible);
-
+        int temperature = 32;
+        if (temperature > 32) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        } else if (temperature > 20) {
+            System.out.println("Beautiful day");
+        } else {
+            System.out.println("Cold day");
+        }
+        /*Simplifying if-statement*/
+        int income = 120_000;
+        boolean hasHighIncome;
+        if (income > 100_000) {
+            hasHighIncome = true;
+        } else {
+            hasHighIncome = false;
+        }
+        /*Above Expression can be simplified by professional be like*/
+        int income = 120_000;
+        boolean hasHighIncome = income > 100_000;
     }
 }
