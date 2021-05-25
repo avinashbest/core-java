b19fb42d20175b169032ecee9b736ceaf04c6e9c
Avinash Kumar <avinashbest3@gmail.com>
2021-05-25 11:43:21 +0530

    Comparision Operator

--- Main.java
+++ Main.java
@@ -1,30 +1,15 @@
 package com.avinashbest;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        /*calculating mortgage*/
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
-
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+        System.out.println(x <= y);
     }
 }
