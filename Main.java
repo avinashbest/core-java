ec28451401032fb418de699eddb370c373217c4b
Avinash Kumar <avinashbest3@gmail.com>
2021-05-25 11:49:24 +0530

    Logical Operators

--- Main.java
+++ Main.java
@@ -3,13 +3,15 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
-        System.out.println(x == y);
-        System.out.println(x != y);
-        System.out.println(x > y);
-        System.out.println(x >= y);
-        System.out.println(x < y);
-        System.out.println(x <= y);
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
+
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasGoodCredit || hasHighIncome) && !hasCriminalRecord;
+        System.out.println(isEligible);
+
     }
 }
