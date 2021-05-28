7a42f171f7e9ceffd5d4c81301135febadb7fad7
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 20:34:00 +0530

    Refactoring Repetitive Patterns: Mortgage Calculator

--- Main.java
+++ Main.java
@@ -1,13 +1,40 @@
 package com.avinashbest;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = greetUser("Avinash", "Kumar");
-        System.out.println(message);
+        int principal = (int) readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate (1% - 30%): ", 1, 30);
+        byte years = (byte) readNumber("Period (1 Years- 30 Years): ", 1, 30);
+
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
     }
 
-    public static String greetUser(String firstname, String lastName) {
-        return "Hello " + firstname + " " + lastName;
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max) {
+                break;
+            }
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        return principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
     }
 }
