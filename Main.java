2674423529ccd01549fc3e70a70e1fcac451303d
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 20:04:59 +0530

    Optimized Mortgage-Calculator

--- Main.java
+++ Main.java
@@ -1,15 +1,54 @@
 package com.avinashbest;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String[] fruits = {"Apple", "Mango", "Orange"};
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println(fruits[i]);
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principal;
+        float annualInterest;
+        float monthlyInterest;
+        byte years;
+        int numberOfPayments;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000) {
+                break;
+            }
+            System.out.println("Enter a value between $1000 and $1,000,000");
         }
-        /*For-Each Loop*/
-        for (var fruit : fruits) {
-            System.out.println(fruit);
+
+        while (true) {
+            System.out.print("Annual Interest Rate (1% - 30%): ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
         }
+
+        while (true) {
+            System.out.print("Period (1 Years- 30 Years): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+
+        }
+        /*calculating mortgage*/
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
     }
 }
