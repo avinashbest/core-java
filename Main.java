57016e78dad6e2dce4997f9493314ff6e749a0ab
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 19:52:27 +0530

    for-each loop

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
 package com.avinashbest;
 
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String input = "";
-
-        while (true) {
-            System.out.print("Input: ");
-            input = scan.next().toLowerCase(Locale.ROOT);
-            if (input.equals("pass")) {
-                continue;/*moves control to the beginning of  the loop*/
-            }
-            if (input.equals("quit")) {
-                break;/*break out of the loop*/
-            }
-            System.out.println(input);
+        String[] fruits = {"Apple", "Mango", "Orange"};
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+        /*For-Each Loop*/
+        for (var fruit : fruits) {
+            System.out.println(fruit);
         }
     }
 }
