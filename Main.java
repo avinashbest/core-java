eb7a9112d7ef5d25c5761e64afe9b2012db624c7
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 19:16:37 +0530

    switch-statement

--- Main.java
+++ Main.java
@@ -3,21 +3,24 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        int income = 120_000;
-        String className;
-        if (income > 100_000) {
-            className = "First";
+        String role = "admin";
+        switch (role) {
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+            case "moderator":
+                System.out.println("You're a moderator");
+                break;
+            default:
+                System.out.println("You're a guest");
+        }
+
+        if (role == "admin") {
+            System.out.println("You're an admin");
+        } else if (role == "moderator") {
+            System.out.println("You're a moderator");
         } else {
-            className = "Economy";
+            System.out.println("You're a guest");
         }
-        /*Simplification can be done as*/
-        income = 120_000;
-        className = "Economy";
-        if (income > 100_000) {
-            className = "First";
-        }
-        /*More Simplification can be done using Ternary Operator*/
-        income = 120_000;
-        className = (income > 100_000) ? "First" : "Economy";
     }
 }
