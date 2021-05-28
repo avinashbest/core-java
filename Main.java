c122aa6eeb68868c8a79fec230c517ba93b79da2
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 19:42:24 +0530

    Loops in Java

--- Main.java
+++ Main.java
@@ -1,25 +1,34 @@
 package com.avinashbest;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
-
-        boolean moduloFive = (number % 5) == 0;
-        boolean moduloThree = (number % 3) == 0;
-
-        if (moduloFive && moduloThree) {
-            System.out.println("FizzBuzz");
-        } else if (moduloFive) {
-            System.out.println("Fizz");
-        } else if (moduloThree) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(number);
+        Scanner scan = new Scanner(System.in);
+        /*FOR LOOPS*/
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello, Java!");
         }
+        /*WHILE LOOPS: USED WHEN DON'T KNOW HOW MANY TIMES WE NEED TO EXECUTE CODE*/
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello, Java!");
+            i++;
+        }
+
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scan.next().toLowerCase(Locale.ROOT);
+            System.out.println(input);
+        }
+        /*DO-WHILE LOOPS: SIMILAR TO WHILE-LOOP BUT IT EXECUTES ATLEAST 1 TIME*/
+        do {
+            System.out.print("Input: ");
+            input = scan.next().toLowerCase(Locale.ROOT);
+            System.out.println(input);
+        } while (!input.equals("quit"));
     }
 }
