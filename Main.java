d78976aa2f05fac14fd789fb373a03674f489c04
Avinash Kumar <avinashbest3@gmail.com>
2021-05-28 20:13:30 +0530

    Creating Method

--- Main.java
+++ Main.java
@@ -1,54 +1,13 @@
 package com.avinashbest;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        String message = greetUser("Avinash", "Kumar");
+        System.out.println(message);
+    }
 
-        int principal;
-        float annualInterest;
-        float monthlyInterest;
-        byte years;
-        int numberOfPayments;
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Principal ($1K - $1M): ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1_000_000) {
-                break;
-            }
-            System.out.println("Enter a value between $1000 and $1,000,000");
-        }
-
-        while (true) {
-            System.out.print("Annual Interest Rate (1% - 30%): ");
-            annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <= 30) {
-                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-
-        while (true) {
-            System.out.print("Period (1 Years- 30 Years): ");
-            years = scanner.nextByte();
-            if (years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-
-        }
-        /*calculating mortgage*/
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
-
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+    public static String greetUser(String firstname, String lastName) {
+        return "Hello " + firstname + " " + lastName;
     }
 }
