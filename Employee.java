7f59264c239fcb57b41750cbc02a2a88b37d346a
Avinash Kumar <avinashbest3@gmail.com>
2021-05-29 22:03:39 +0530

    Getters & Setters

--- Employee.java
+++ Employee.java
@@ -1,11 +1,34 @@
 package com.avinashbest;
 
 public class Employee {
-    public int baseSalary;
-//    public int extraHours;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
+    }
+
+    /*SETTERS*/
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Salary can't be 0 or less.");
+        }
+        this.baseSalary = baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly rate can't be 0 or less.");
+        }
+        this.hourlyRate = hourlyRate;
+    }
+
+    /*GETTERS*/
+    public int getBaseSalary() {
+        return this.baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return this.hourlyRate;
     }
 }
--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ package com.avinashbest;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+
+        employee.setBaseSalary(50_000);
+        System.out.println(employee.getBaseSalary());
+
+        employee.setHourlyRate(100);
+        System.out.println(employee.getHourlyRate());
 
         int wage = employee.calculateWage(10);
         System.out.println(wage);
