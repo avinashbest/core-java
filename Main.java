d58827caccfe9cf1922ad7dbcbb726982ca773e1
Avinash Kumar <avinashbest3@gmail.com>
2021-05-29 20:17:40 +0530

    Procedural Programming

--- Main.java
+++ Main.java
@@ -2,9 +2,15 @@ package com.avinashbest;
 
 public class Main {
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        var textBox2 = textBox1;
-        textBox2.setText("Hello, Box 2");
-        System.out.println(textBox1.text);
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
+
+        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        System.out.println(wage);
+    }
+
+    public static int calculateWage(int baseSalary, int extraHours, int hourlyRate) {
+        return baseSalary + (extraHours * hourlyRate);
     }
 }
