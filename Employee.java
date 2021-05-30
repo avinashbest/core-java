0dde2fa9b23e84832d2514368b6cfdae18decba8
Avinash Kumar <avinashbest3@gmail.com>
2021-05-30 23:09:37 +0530

    Constructors

--- Employee.java
+++ Employee.java
@@ -4,19 +4,25 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    /*CONSTRUCTORS*/
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
     }
 
     /*SETTERS*/
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0) {
             throw new IllegalArgumentException("Salary can't be 0 or less.");
         }
         this.baseSalary = baseSalary;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if (hourlyRate <= 0) {
             throw new IllegalArgumentException("Hourly rate can't be 0 or less.");
         }
--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ package com.avinashbest;
 
 public class Main {
     public static void main(String[] args) {
-        var browser = new Browser();
-        browser.navigate("https://www.google.com");
+        var employee = new Employee(50_000, 20);
+
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
     }
 }
