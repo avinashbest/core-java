ba4c15834c718a42ba98afba8908bd31a6a4db87
Avinash Kumar <avinashbest3@gmail.com>
2021-05-31 19:30:59 +0530

    Mortagage Calculator: One Last Touch

--- MortgageReport.java
+++ MortgageReport.java
@@ -4,10 +4,12 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -15,13 +17,13 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("----------------");
         for (double balance : calculator.getRemainingBalances()) {
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
         }
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
