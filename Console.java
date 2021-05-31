1f7986e84be229afe52ff9afb35d41041d4b94a2
Avinash Kumar <avinashbest3@gmail.com>
2021-05-31 18:42:40 +0530

    Mortagage Calculator: Overloading Method readNumber of Console class

--- Console.java
+++ Console.java
@@ -3,12 +3,17 @@ package com.avinashbest;
 import java.util.Scanner;
 
 public class Console {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double readNumber(String prompt) {
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
         double value;
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextFloat();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " and " + max);
