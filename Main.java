973a93ca7d7408319596abd7be4066b3b0008df3
Avinash Kumar <avinashbest3@gmail.com>
2021-06-01 19:20:49 +0530

    Mortagage Calculator: One Last Touch

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ package com.avinashbest;
 public class Main {
 
     public static void main(String[] args) {
-        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
+        int principal = (int) Console.readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate (1% - 30%): ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (1 Years- 30 Years): ", 1, 30);
 
         var calculator = new MortgageCalculator(principal, annualInterest, years);
 
