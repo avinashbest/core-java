81f3704cee40d559be5f973df0fbed60f2c457be
Avinash Kumar <avinashbest3@gmail.com>
2021-06-01 22:02:46 +0530

    Final classes & Methods

--- CheckBox.java
+++ CheckBox.java
@@ -1,6 +1,8 @@
 package com.avinashbest;
 
-public class CheckBox extends UIControl {
+//if a class is declared 'final' then no other class can inherit it.
+//an example of such type class is String class in java, which is declared as final in its implementation, this is done because Strings in java are immutable.
+public final class CheckBox extends UIControl {
     @Override
     public void render() {
         System.out.println("Render CheckBox");
